f83cc6ca573fa23eb077da4a13756fdc57be7f63
Parmenas Munene <parmenasmunene165@gmail.com>
2023-05-18 23:43:24 +0300

    wip

---
+++ frontend/voting/hardhat.config.js
@@ -0,0 +1,22 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.11",
+   defaultNetwork: "volta",
+   networks: {
+      hardhat: {},
+      volta: {
+         // url: add this,
+         // accounts: add this,
+         // gas: add this,
+         // gasPrice: add this,
+      }
+   },
+}
